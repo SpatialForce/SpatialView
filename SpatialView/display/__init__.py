d32eb727b7dcb74ebf7f26bd928c72750919af73
yangfengzzz <yangfengzzz@hotmail.com>
2024-03-11 07:44:32 +0800

    save

--- SpatialView/display/__init__.py
+++ SpatialView/display/__init__.py
@@ -1,4 +1,5 @@
+from .vtk_renderer import Renderer
 from .vtk_renderer_model import VtkRendererSettingsModel
 from .vtk_skybox_model import VtkSkyboxModel
-from .vtk_display_actor_model import VtkDisplayActorModel
-from .vtk_renderer import Renderer
+from .vtk_actor_model import VtkDisplayActorModel
+from .vtk_text_actor import VtkTextActorModel
