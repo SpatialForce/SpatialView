fd7655a157fa7521aaa1c458e7f0821a3fa273d0
yangfengzzz <yangfengzzz@hotmail.com>
2024-03-10 22:03:12 +0800

    save

--- SpatialView/reader/__init__.py
+++ SpatialView/reader/__init__.py
@@ -1,5 +1,7 @@
 from .vtk_exodusII_reader_model import VtkExodusIIReaderModel
 from .vtk_slc_reader_model import VtkSLCReaderModel
+from .vtk_ply_reader_model import VtkPLYReaderModel
+from .vtk_stl_reader_model import VtkSTLReaderModel
 from .vtk_hdr_reader_model import VtkHDRReaderModel
 from .vtk_xml_image_data_reader_model import VtkXMLImageDataReaderModel
 from .vtk_xml_poly_data_reader_model import VtkXMLPolyDataReaderModel
